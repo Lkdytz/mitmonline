d8786091893e8d8dc91cda9eb997b1a5c45e8e01
Lkdytz <88686694+Lkdytz@users.noreply.github.com>
2026-07-26 22:08:33 -0400

    Delete src directory

--- src/index.js
+++
@@ -1,8 +0,0 @@
-const { readFileSync } = require('fs')
-const { join } = require('path')
-
-module.exports = (req, res) => {
-  const html = readFileSync(join(__dirname, '..', 'index.html'), 'utf8')
-  res.setHeader('Content-Type', 'text/html; charset=utf-8')
-  res.end(html)
-}
